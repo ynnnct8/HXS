2592fa73964de9b9b6952db9b7a719de62bd63ab
Gemiigne <ynnnct@outlook.com>
2024-03-04 21:22:55 +0800

    init

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式不正确");
+        }
+        // 3.如果符合，发送验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 4.将验证码保存到session中
+        session.setAttribute("code", code);
+        // 5.发送验证码
+        log.debug("验证码发送成功（模拟）");
+        // 6.返回成功信息
+        return Result.ok();
+    }
+}
